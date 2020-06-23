acbe85928c79ebde213b78d7a03869a5afbd2507
smsqadri <smsqadri@gmail.com>
2020-06-23 15:12:42 +0300

    Update Breakfast.js

--- src/Breakfast.js
+++ src/Breakfast.js
@@ -4,7 +4,7 @@ function Breakfast (props){
     return (
         <div>
             <h1>Today's Breakfast we are serving {props.DishName}</h1>
-            <h1>We also Serve Sweet {props.Sweet}</h1>
+            <h2>We also Serve Sweet {props.Sweet}</h2>
             
         </div>
     )
